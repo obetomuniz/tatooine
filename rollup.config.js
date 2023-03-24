7ca785fffe09981d93a2adc8e4ec4856f6943821
Beto Muniz <contato@betomuniz.com>
2023-03-24 12:14:53 -0300

    feat(release): Introduce release-it to help with releasing management

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,20 @@ import commonjs from "@rollup/plugin-commonjs"
 import typescript from "@rollup/plugin-typescript"
 import dts from "rollup-plugin-dts"
 
-// import filesize from "rollup-plugin-filesize"
-// import packageJson from "./package.json" assert { type: "json" }
+import filesize from "rollup-plugin-filesize"
+import packageJson from "./package.json" assert { type: "json" }
 
 const config = [
   {
     input: "lib/index.ts",
     output: [
       {
-        file: "./dist/esm/index.js",
+        file: packageJson.module,
         format: "esm",
         sourcemap: true,
       },
       {
-        file: "./dist/cjs/index.js",
+        file: packageJson.main,
         format: "cjs",
         sourcemap: true,
       },
@@ -27,6 +27,7 @@ const config = [
       typescript({
         tsconfig: "./tsconfig.json",
       }),
+      filesize(),
     ],
     external: ["axios", "jsdom", "xpath-ts", "puppeteer", "lodash.get"],
   },
